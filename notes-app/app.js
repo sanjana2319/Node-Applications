a644cff2119269e3b51362a2aa1a04a17816ed99
Sanjana Jain <2319sanjana@gmail.com>
2020-09-24 02:30:58 +0530

    commit updation

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,52 @@
 // const validator = require("validator");
 const chalk = require("chalk");
+const { argv } = require("yargs");
+const yargs = require("yargs");
 
-const getNotes = require("./notes.js");
+yargs.version("1.1.0");
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string",
+        },
+        body: {
+            describe: "Note body content",
+            demandOption: true,
+            type: "string",
+        },
+    },
+    handler: function (argv) {
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
+    },
+});
 
-const msg = getNotes();
-console.log(msg);
-// console.log(validator.isURL("sanjana.com"));
-console.log(chalk.bold.inverse.red("Success!"));
+yargs.command({
+    command: "remove",
+    describe: "Remove the note",
+    handler: function () {
+        console.log("Remove the note");
+    },
+});
+
+yargs.command({
+    command: "list",
+    describe: "Displaying list",
+    handler: function () {
+        console.log("List:");
+    },
+});
+
+yargs.command({
+    command: "read",
+    describe: "Reading the list",
+    handler: function () {
+        console.log("Reading the list");
+    },
+});
+yargs.parse();
+// console.log(yargs.argv);
