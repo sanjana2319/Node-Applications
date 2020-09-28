c37e0536468524d4ca3e38c6e405520707363bb7
Sanjana Jain <2319sanjana@gmail.com>
2020-09-29 02:43:00 +0530

    es6 objects- pratice

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,27 @@
+const name = "Sanjana";
+const userAge = "21";
+
+const user = {
+    name,
+    age: userAge,
+    location: "India",
+};
+
+console.log(user);
+
+const products = {
+    type: "Single-lined",
+    pages: 100,
+    stock: 500,
+    price: 30,
+    salePrice: undefined,
+};
+
+// const { type: productType, stock } = products;
+// console.log(productType);
+// console.log(stock);
+
+const transaction = (productMaterial, { type, stock }) => {
+    console.log(productMaterial, type, stock);
+};
+transaction("Smooth", products);
