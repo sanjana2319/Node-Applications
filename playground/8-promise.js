49fb9427d13f08c54725dade4bfe85f92f330eba
Sanjana Jain <2319sanjana@gmail.com>
2020-10-04 17:17:14 +0530

    promises

--- playground/8-promise.js
+++ playground/8-promise.js
@@ -1,14 +1,34 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        reject("Not Happening!");
-        resolve([2, 3, 5]);
-    }, 2000);
-});
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         reject("Not Happening!");
+//         resolve([2, 3, 5]);
+//     }, 2000);
+// });
 
-doWorkPromise
-    .then((result) => {
-        console.log("Success!", result);
+// doWorkPromise
+//     .then((result) => {
+//         console.log("Success!", result);
+//     })
+//     .catch((error) => {
+//         console.log("Error: ", error);
+//     });
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    });
+};
+
+add(2, 3)
+    .then((sum) => {
+        console.log(sum);
+        return add(sum, 2);
     })
-    .catch((error) => {
-        console.log("Error: ", error);
+    .then((sum2) => {
+        console.log(sum2);
+    })
+    .catch((e) => {
+        console.log(e);
     });
