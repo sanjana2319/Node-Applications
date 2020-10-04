3549570bb558c885620c84a9452b2f307faf2533
Sanjana Jain <2319sanjana@gmail.com>
2020-10-04 17:19:17 +0530

    handelling promise chaning in the app

--- task-manager/playground/promise-chaning-2.js
+++ task-manager/playground/promise-chaning-2.js
@@ -1,13 +1,28 @@
 require("../src/db/mongoose");
+const { count } = require("../src/models/task");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("5f76374868cdb93dfcf87ced").then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false })
-        .then((result) => {
-            console.log(result);
-        })
-        .catch((e) => {
-            console.log(e);
-        });
-});
+// Task.findByIdAndDelete("5f76374868cdb93dfcf87ced").then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false })
+//         .then((result) => {
+//             console.log(result);
+//         })
+//         .catch((e) => {
+//             console.log(e);
+//         });
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count;
+};
+
+deleteTaskAndCount("5f76188e5cb7e13350576c1c")
+    .then((count) => {
+        console.log(count);
+    })
+    .catch((e) => {
+        console.log(e);
+    });
